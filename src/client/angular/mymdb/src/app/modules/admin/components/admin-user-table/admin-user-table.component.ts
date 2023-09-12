2341b67ab22ae2a1e46f0b17eeed1d1d5396bd76
djek-sweng <djek@gmx.ch>
2023-09-12 21:02:16 +0200

    Angular

---
+++ src/client/angular/mymdb/src/app/modules/admin/components/admin-user-table/admin-user-table.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { IAdminUser } from '../../models/admin-user.model';
+import { AdminUserService } from '../../services/admin-user.service';
+
+@Component({
+  selector: 'app-admin-user-table',
+  templateUrl: './admin-user-table.component.html',
+  styleUrls: ['./admin-user-table.component.css'],
+})
+export class AdminUserTableComponent implements OnInit {
+  users$ = new Observable<IAdminUser[]>();
+
+  constructor(private readonly adminUserService: AdminUserService) {}
+
+  ngOnInit(): void {
+    this.adminUserService.loadUsers();
+    this.users$ = this.adminUserService.getUsers$();
+  }
+
+  OnDelete(id: string | undefined): void {
+    if (!id) {
+      return;
+    }
+
+    this.adminUserService.deleteUser(id);
+  }
+}
---
+++ src/client/angular/mymdb/src/app/modules/auth/store/auth.actions.ts
@@ -0,0 +1,77 @@
+import { createAction, props } from "@ngrx/store";
+
+import { User } from "../../shared/models/user.model";
+import { IHttpError } from "../../shared/models/http-error.model";
+
+/**
+ * SIGN_UP_START
+ */
+interface ISignUpStartProps {
+  email: string;
+  password: string;
+};
+
+export const signUpStart = createAction(
+  '[Auth] Sign Up Start',
+  props<ISignUpStartProps>()
+);
+
+/**
+ * AUTO_SIGN_IN
+ */
+export const autoSignIn = createAction(
+  '[Auth] Auto Sign In'
+);
+
+/**
+ * SIGN_IN_START
+ */
+interface ISignInStartProps {
+  email: string;
+  password: string;
+};
+
+export const signInStart = createAction(
+  '[Auth] Sign In Start',
+  props<ISignInStartProps>()
+);
+
+/**
+ * AUTHENTICATE_SUCCESS
+ */
+interface IAuthenticateSuccessProps {
+  user: User;
+  redirect: boolean;
+};
+
+export const authenticateSuccess = createAction(
+  '[Auth] Authenticate Success',
+  props<IAuthenticateSuccessProps>()
+);
+
+/**
+ * AUTHENTICATE_ERROR
+ */
+interface IAuthenticateErrorProps {
+  error: IHttpError;
+};
+
+export const authenticateError = createAction(
+  '[Auth] Authenticate Error',
+  props<IAuthenticateErrorProps>()
+);
+
+/**
+ * SIGN_OUT
+ */
+export const signOut = createAction(
+  '[Auth] Sign Out'
+);
+
+/**
+ * RESET_ERROR
+ */
+
+export const resetError = createAction(
+  '[Auth] Reset Error'
+);
